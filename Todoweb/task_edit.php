51d9ed1038b63788e1f6f7f4f806b2b8bf428c53
MartinOfficial <47602439+MartinOfficial@users.noreply.github.com>
2024-09-05 11:08:14 +0200

    apró módosítás

--- Todoweb/task_edit.php
+++ Todoweb/task_edit.php
@@ -57,7 +57,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feladat módosítása</title>
+    <title>Teendők - Feladat módosítása</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
